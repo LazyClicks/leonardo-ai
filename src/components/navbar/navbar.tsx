42ab29d25ca64c1501f90075b3b68526e6d4981b
Kermout Ayoub <ayoub.kermout@etu.usthb.dz>
2023-09-05 22:44:35 +0100

    update

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,11 +1,21 @@
 import { component$ } from "@builder.io/qwik";
+import ImgLogo from "~/assets/images/logo.svg";
 
 export default component$(() => {
   return (
     <>
       <div class="navbar bg-info shadow-lg">
         <div class="flex-1">
-          <a class="btn btn-ghost normal-case text-xl">Leonardo.Ai</a>
+          <a class="btn btn-ghost normal-case text-xl">
+            <img
+              src={ImgLogo}
+              alt="Logo"
+              height={50}
+              width={50}
+              class="w-7 h-7"
+            />
+            Leonardo.Ai
+          </a>
         </div>
         <div class="flex-none">
           <button class="btn btn-primary rounded-full capitalize font-neov w-32 ">
